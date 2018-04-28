a4310f1db77474f6ee199051154c3b93412af5f7
smatsumo2019 <smatsumo2019@falmouthschools.org>
2018-04-28 17:24:09 -0400

    oage

---
+++ app/src/main/java/org/hackme/presence/AttendanceSheet.java
@@ -0,0 +1,13 @@
+package org.hackme.presence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttendanceSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_sheet);
+    }
+}
