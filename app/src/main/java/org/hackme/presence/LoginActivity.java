ef17d3ac77d801b2532991b7e91237c28782090f
Luke Bodwell <lbodwell2018@falmouthschools.org>
2018-04-28 13:06:03 -0400

    Push name to login

--- app/src/main/java/org/hackme/presence/LoginActivity.java
+++ app/src/main/java/org/hackme/presence/LoginActivity.java
@@ -2,16 +2,18 @@ package org.hackme.presence;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    public String studentName;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
-        //TODO
+        studentName = getIntent().getStringExtra("STUDENT_NAME");
+        Log.d("LOGIN", "made it to login");
     }
 
-    public void onClick(View view) {
-        //TODO
+    public void onClick(View v) {
     }
 }
