d34c18c87728e866138628d19f798607768b8e41
Luke Bodwell <lbodwell2018@falmouthschools.org>
2018-04-28 12:03:55 -0400

    Fix button problems

---
+++ app/src/main/java/org/hackme/presence/LoginActivity.java
@@ -0,0 +1,17 @@
+package org.hackme.presence;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_list);
+        //TODO
+    }
+
+    public void onClick(View view) {
+        //TODO
+    }
+}
--- app/src/main/java/org/hackme/presence/NameListActivity.java
+++ app/src/main/java/org/hackme/presence/NameListActivity.java
@@ -6,25 +6,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class NameListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class NameListActivity extends AppCompatActivity implements View.OnClickListener {
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public String name;
+    public final Intent login = new Intent(this, LoginActivity.class);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_name_list);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
 
-        final Intent startRandP = new Intent(this, EnterPeriodRoom.class);
-
-        Button submitButton = findViewById(R.id.startButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                startActivity(startRandP);
-
-
-            }
-
-        });
-
-
+    public void onClick(View v) {
+        Button b = (Button) v;
+        name = b.getText().toString();
+        //startActivity(login);
     }
 }
