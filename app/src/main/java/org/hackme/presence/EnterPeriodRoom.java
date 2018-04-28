6aa9a3bd0b69cb28d1090e50560e024916715d91
smatsumo2019 <smatsumo2019@falmouthschools.org>
2018-04-28 14:15:07 -0400

    added a spinnah
    spinnah got code

--- app/src/main/java/org/hackme/presence/EnterPeriodRoom.java
+++ app/src/main/java/org/hackme/presence/EnterPeriodRoom.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnterPeriodRoom extends AppCompatActivity {
 
@@ -14,8 +20,24 @@ public class EnterPeriodRoom extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_period_room);
 
-        //final EditText roomNumberInput = (EditText)findViewById(R.id.roomInput);
-        //final EditText periodNumberInput = (EditText)findViewById(R.id.periodInput);
+        final EditText roomNumberInput = (EditText)findViewById(R.id.roomInput);
+        final EditText periodNumberInput = (EditText)findViewById(R.id.periodInput);
+
+        final Spinner scheduleDay = (Spinner) findViewById(R.id.scheduleDaySpinner);
+
+        List<String> dayLetters = new ArrayList<>();
+        dayLetters.add("A");
+        dayLetters.add("B");
+        dayLetters.add("C");
+        dayLetters.add("D");
+        dayLetters.add("E");
+        dayLetters.add("Ax");
+        dayLetters.add("Bx");
+        dayLetters.add("Cx");
+
+        ArrayAdapter<String> dayLettersArray = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,dayLetters);
+
+        scheduleDay.setAdapter(dayLettersArray);
 
         Button submitButton = findViewById(R.id.submitButton);
 
@@ -24,8 +46,12 @@ public class EnterPeriodRoom extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                //int roomNumberInt = Integer.parseInt(roomNumberInput.getText().toString());
-                //int periodNumberInt = Integer.parseInt(periodNumberInput.getText().toString());
+                int roomNumberInt = Integer.parseInt(roomNumberInput.getText().toString());
+                int periodNumberInt = Integer.parseInt(periodNumberInput.getText().toString());
+
+                Toast.makeText(EnterPeriodRoom.this,
+                        "" + roomNumberInt,
+                        Toast.LENGTH_LONG).show();
 
                 startActivity(goToNameList);
 
