acbce34a773f7135e8c44fdeace6d93c34840565
smatsumo2019 <smatsumo2019@falmouthschools.org>
2018-04-28 11:03:20 -0400

    My currnt version
    has buttons and screens for starting up and takes input of the numbers

---
+++ app/src/main/java/org/hackme/presence/EnterPeriodRoom.java
@@ -0,0 +1,30 @@
+package org.hackme.presence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EnterPeriodRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_period_room);
+
+        final EditText roomNumberInput = (EditText)findViewById(R.id.roomInput);
+        final EditText periodNumberInput = (EditText)findViewById(R.id.periodInput);
+
+        Button submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                int roomNumberInt = Integer.parseInt(roomNumberInput.getText().toString());
+                int periodNumberInt = Integer.parseInt(periodNumberInput.getText().toString());
+
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/org/hackme/presence/StartActivity.java
@@ -0,0 +1,31 @@
+package org.hackme.presence;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        final Intent startRandP = new Intent(this, EnterPeriodRoom.class);
+
+        Button submitButton = findViewById(R.id.startButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                startActivity(startRandP);
+
+
+            }
+
+        });
+
+
+    }
+}
