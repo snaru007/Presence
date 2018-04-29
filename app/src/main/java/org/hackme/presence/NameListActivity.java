45c0a641f9c64a7e6bf7de93b09c6a77280a5d9a
Shubhankar Naru <snaru2018@falmouthschools.org>
2018-04-28 17:10:40 -0700

    added period time stuff

--- app/src/main/java/org/hackme/presence/NameListActivity.java
+++ app/src/main/java/org/hackme/presence/NameListActivity.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class NameListActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,17 +20,23 @@ public class NameListActivity extends AppCompatActivity implements View.OnClickL
     public Button button3;
     //public List<Button> buttons;
     public String name;
+    public Calendar calendar;
+    private int roomNumber;
+    private int periodNumber;
+    private String dayType = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
+        calendar = new GregorianCalendar();
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
-        int roomNumber = getIntent().getIntExtra("ROOM_NUMBER", 0);
-        int periodNumber = getIntent().getIntExtra("PERIOD_NUMBER", 0);
-        String  dayType = getIntent().getStringExtra("DAY_LETTER");
+        roomNumber = getIntent().getIntExtra("ROOM_NUMBER", 0);
+        periodNumber = getIntent().getIntExtra("PERIOD_NUMBER", 0);
+        dayType = getIntent().getStringExtra("DAY_LETTER");
 
         //buttons.add(button1);
         //buttons.add(button2);
@@ -50,6 +58,42 @@ public class NameListActivity extends AppCompatActivity implements View.OnClickL
         button3.setText(name);
         Intent login = new Intent(getBaseContext(), LoginActivity.class);
         login.putExtra("STUDENT_NAME", name);
+        int currentMin = calendar.get(Calendar.MINUTE);
+        int currentHour = calendar.get(Calendar.HOUR);
+
+        Toast.makeText(NameListActivity.this,currentHour +":"+ currentMin, Toast.LENGTH_LONG).show();
+        boolean isTardy = false;
+
+        switch(dayType) {
+            case "A":
+                switch(periodNumber) {
+                    case 1:
+                        isTardy = !(currentHour < 8 || (currentHour == 8 && currentMin <= 2));
+                        break;
+                    case 2:
+                        isTardy = !(currentHour < 9 || (currentHour == 9 && currentMin <= 30));
+                        break;
+                    case 3:
+                        String rn = roomNumber + "";
+                        if(rn.charAt(0) == '1') {
+                            isTardy = !(currentHour < 11 || (currentHour == 11 && currentMin <= 17));
+                        } else if(rn.charAt(0) == '2') {
+                            isTardy = !(currentHour < 10 || (currentHour == 10 && currentMin <= 52));
+                        }
+                        break;
+                    case 4:
+                        isTardy = !(currentHour < 1 || (currentHour == 1 && currentMin <= 4));
+                        break;
+                    default: break;
+                }
+            case "AX":
+                break;
+            case "B":
+                break;
+            case "BX":
+                break;
+        }
+        login.putExtra("Tardy", isTardy);
         startActivity(login);
         Log.d("NAME", name);
     }
